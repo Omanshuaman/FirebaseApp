6eef48502d7df1fffbfaede6a6f34146f24b8ce8
Omanshu Aman <omanshuaman@gmail.com>
2022-05-16 22:15:41 +0530

    Firebase Social Media App - 03 Recover Forgotten Password
    Firebase Social Media App - 03 Recover Forgotten Password

--- app/src/main/java/com/example/firebaseapp2/LoginActivity.java
+++ app/src/main/java/com/example/firebaseapp2/LoginActivity.java
@@ -4,13 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
 
     //progress dialog
     ProgressDialog pd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,12 +65,11 @@ public class LoginActivity extends AppCompatActivity {
             //input data
             String email = mEmailEt.getText().toString();
             String passw = mPasswordEt.getText().toString().trim();
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 //invalid email pattern set error
                 mEmailEt.setError("Invalid Email");
                 mEmailEt.setFocusable(true);
-            }
-            else {
+            } else {
                 //valid email pattern
                 loginUser(email, passw);
             }
@@ -75,10 +79,85 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             finish();
         });
+        //recover pass textview click
+        mRecoverPassTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRecoverPasswordDialog();
+            }
+        });
         //init progress dialog
         pd = new ProgressDialog(this);
     }
 
+    private void showRecoverPasswordDialog() {
+        //AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Recover Password");
+
+        //set layout linear layout
+        LinearLayout linearLayout = new LinearLayout(this);
+        //views to set in dialog
+        final EditText emailEt = new EditText(this);
+        emailEt.setHint("Email");
+        emailEt.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        /*sets the min width of a EditView to fit a text of n 'M' letters regardless of the actual text
+        extension and text size.*/
+        emailEt.setMinEms(16);
+
+
+        linearLayout.addView(emailEt);
+        linearLayout.setPadding(10, 10, 10, 10);
+
+        builder.setView(linearLayout);
+
+        //buttons recover
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //input email
+                String email = emailEt.getText().toString().trim();
+                beginRecovery(email);
+            }
+        });
+        //buttons cancel
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //dismiss dialog
+                dialog.dismiss();
+            }
+        });
+
+        //show dialog
+        builder.create().show();
+    }
+
+    private void beginRecovery(String email) {
+        //show progresses dialog
+        pd.setMessage("Sending email...");
+        pd.show();
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        pd.dismiss();
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Email sent", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "Failed...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                //get and show proper error message
+                Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     private void loginUser(String email, String passw) {
         //show progresses dialog
         pd.setMessage("Logging In...");
@@ -109,7 +188,7 @@ public class LoginActivity extends AppCompatActivity {
                 //dismiss progress dialog
                 pd.dismiss();
                 //error, get and show error message
-                Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
