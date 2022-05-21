fd6633f21a727a3dc5957b4b7f33782414d0e2e7
Omanshu Aman <omanshuaman@gmail.com>
2022-05-21 21:41:20 +0530

    Firebase Social Media App - 14 Typing Status in Chat

--- app/src/main/java/com/example/firebaseapp2/ChatActivity.java
+++ app/src/main/java/com/example/firebaseapp2/ChatActivity.java
@@ -7,7 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
@@ -41,7 +43,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class ChatActivity extends AppCompatActivity {
-    private static final String TAG ="FUNK YOU" ;
+    private static final String TAG = "FUNK YOU";
     //views from xml
     Toolbar toolbar;
     RecyclerView recyclerView;
@@ -114,6 +116,11 @@ public class ChatActivity extends AppCompatActivity {
                     hisImage = "" + ds.child("image").getValue();
                     String typingStatus = "" + ds.child("typingTo").getValue();
 
+                    //check typing status
+                    if (typingStatus.equals(myUid)) {
+                        userStatusTv.setText("typing...");
+                    } else {
+                        //get value of onlinestatus
                         String onlineStatus = "" + ds.child("onlineStatus").getValue();
                         if (onlineStatus.equals("online")) {
                             userStatusTv.setText(onlineStatus);
@@ -126,7 +133,7 @@ public class ChatActivity extends AppCompatActivity {
                             userStatusTv.setText("Last seen at: " + dateTime);
 
                         }
-
+                    }
                     //set data
                     nameTv.setText(name);
                     try {
@@ -158,12 +165,34 @@ public class ChatActivity extends AppCompatActivity {
                     //text not empty
                     sendMessage(message);
                 }
+            }
+        });
+        //check edit text change listener
+        messageEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() == 0) {
+                    checkTypingStatus("noOne");
+                } else {
+                    checkTypingStatus(hisUid);//uid of receiver
                 }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
         });
         readMessages();
         seenMessage();
 
     }
+
     private void seenMessage() {
         userRefForSeen = FirebaseDatabase.getInstance().getReference("Chats");
         seenListener = userRefForSeen.addValueEventListener(new ValueEventListener() {
@@ -244,6 +273,7 @@ public class ChatActivity extends AppCompatActivity {
 
 
     }
+
     private void checkUserStatus() {
         //get current user
         FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -251,13 +281,14 @@ public class ChatActivity extends AppCompatActivity {
             //user is signed in stay here
             //set email of logged in user
             //mProfileTv.setText(user.getEmail());
-            myUid=user.getUid(); //currently signed in user's uid
+            myUid = user.getUid(); //currently signed in user's uid
         } else {
             //user not signed in, go to main activity
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
     }
+
     private void checkOnlineStatus(String status) {
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users").child(myUid);
         HashMap<String, Object> hashMap = new HashMap<>();
@@ -265,6 +296,15 @@ public class ChatActivity extends AppCompatActivity {
         //update value of onlineStatus of current user
         dbRef.updateChildren(hashMap);
     }
+
+    private void checkTypingStatus(String typing) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users").child(myUid);
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("typingTo", typing);
+        //update value of onlineStatus of current user
+        dbRef.updateChildren(hashMap);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -285,6 +325,7 @@ public class ChatActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     @Override
     protected void onStart() {
         checkUserStatus();
@@ -292,6 +333,7 @@ public class ChatActivity extends AppCompatActivity {
         checkOnlineStatus("online");
         super.onStart();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -299,8 +341,10 @@ public class ChatActivity extends AppCompatActivity {
         String timestamp = String.valueOf(System.currentTimeMillis());
         //set offline with last seen time stamp
         checkOnlineStatus(timestamp);
+        checkTypingStatus("noOne");
         userRefForSeen.removeEventListener(seenListener);
     }
+
     @Override
     protected void onResume() {
         //set online
